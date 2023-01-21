84b068f55f0694d430b1a48b34dedd2e919fd13a
Frank Morton <fmorton@mac.com>
2023-01-21 16:55:52 -0500

    try making led 1 blink

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 hummingbird.startHummingbird()
 basic.forever(function () {
-	
+    hummingbird.setLED(ThreePort.One, 50)
+    basic.pause(500)
+    hummingbird.setLED(ThreePort.One, 0)
+    basic.pause(500)
 })
